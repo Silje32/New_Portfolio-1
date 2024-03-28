e7b90c526e2f10b456abac88960e69427a843eb4
Silje Heldal Monsen <silje@Silje-sin-MacBook-Air.local>
2024-03-28 13:56:17 +0100

    Updated the styles of my cards

--- src/components/Cards/styledCard.styles.jsx
+++ src/components/Cards/styledCard.styles.jsx
@@ -1,13 +1,49 @@
 import styled from "styled-components";
 
-export const StyledHomeCard = styled.div`
+export const StyledCard = styled.div`
    display: flex;
-   flex-direction: column;
+   flex: 1;
+   flex-direction: row;
    align-items: center;
    background-color: white;
    border-radius: 5px;
-   margin: 20px 130px;
    padding: 60px;
+`
+
+export const StyledExamCard = styled.div`
+   display: flex;
+   flex: 1;
+   flex-direction: column;
+   align-items: center;
+   background-color: #5EA7EF;
+   border-radius: 5px;
+   border-color: 2px black;
+   padding: 60px;
+   margin-left: 25px;
+`
+
+export const StyledRecycleCard = styled.div`
+   display: flex;
+   flex: 1;
+   flex-direction: column;
+   align-items: center;
+   background-color: #5EA7EF;
+   border-radius: 5px;
+   border-color: 2px black;
+   padding: 60px;
+   margin-left: 25px;
+`
+
+export const StyledProjectCard = styled.div`
+   display: flex;
+   flex: 1;
+   flex-direction: column;
+   align-items: center;
+   background-color: #5EA7EF;
+   border-radius: 5px;
+   border-color: 2px black;
+   padding: 60px;
+   margin-left: 25px;
 `
 ;
 
